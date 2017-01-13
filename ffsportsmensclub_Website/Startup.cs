bfd8e9e24099da4099e6b68a461738239d6f00c4
TBritton <tbritton@confederationc.on.ca>
2017-01-13 12:43:03 -0500

    Add project files.

---
+++ ffsportsmensclub_Website/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ffsportsmensclub_Website.Startup))]
+namespace ffsportsmensclub_Website
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
