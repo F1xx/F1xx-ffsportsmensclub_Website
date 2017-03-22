a2b0db3c5de339425c0cb894cad2718ba2f6f871
applecheeks <dmille10@confederationc.on.ca>
2017-03-22 09:47:13 -0500

    Changed the nav bar a bit, did the other slight changes

--- ffsportsmensclub_Website/Account/Login.aspx.designer.cs
+++ ffsportsmensclub_Website/Account/Login.aspx.designer.cs
@@ -65,14 +65,5 @@ namespace ffsportsmensclub_Website.Account {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.HyperLink RegisterHyperLink;
-        
-        /// <summary>
-        /// OpenAuthLogin control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::ffsportsmensclub_Website.Account.OpenAuthProviders OpenAuthLogin;
     }
 }
